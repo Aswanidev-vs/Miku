dc896ab26d4d204f6ad9ab05278d31e162521f92
Aswanidev <apploginer123@gmail.com>
2026-08-29 01:27:24 +0530

    chore: bump to v0.12.5

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Version is the current app version. Bump before each release build.
-const Version = "0.12.4"
+const Version = "0.12.5"
 
 //go:embed all:frontend/dist
 var assets embed.FS
